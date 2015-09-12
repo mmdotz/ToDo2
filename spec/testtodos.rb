fbec5db80414057beeeee2db003d2ac1e89bdd14
Michelle Dotzenrod <dotzenrodm@gmail.com>
2015-09-12 18:00:20 -0500

    add todo working

--- spec/testtodos.rb
+++ spec/testtodos.rb
@@ -5,11 +5,14 @@ require_relative '../bin/todo.rb'
 
 class TestTodos < Minitest::Test
 
-def test_class_todo
-  assert(Todo)
-end
+  def test_class_todo
+    assert(Todo)
+  end
 
-def test
+  def test_add_todo
+    assert_equal("clean kitchen", Todo.new.add_todo("clean kitchen"))
+  end
+
 
 
 
