83990f9cecece2f2926ab329836380bd04e4e74a
Michelle Dotzenrod <dotzenrodm@gmail.com>
2015-09-12 18:40:17 -0500

    re-migrated after changing Tasklist to Todo

--- bin/todo.rb
+++ bin/todo.rb
@@ -1,6 +1,17 @@
 class Todo
 
-def greeting
+  def add_todo
+    Todo.new(name: task)
+  end
+
+  def prompt_for_task
+    puts "What task would you like to add to your list?"
+    task = gets.chomp
+    puts "You have added #{task} to your list."
+  end
 end
 
-end
+
+# Todo.new.prompt_for_task
+
+
--- spec/testtodos.rb
+++ spec/testtodos.rb
@@ -6,13 +6,23 @@ require_relative '../bin/todo.rb'
 class TestTodos < Minitest::Test
 
   def test_class_todo
+    skip
     assert(Todo)
   end
 
   def test_add_todo
+    skip
     assert_equal("clean kitchen", Todo.new.add_todo("clean kitchen"))
   end
 
+  def test_prompt
+    skip
+    assert_equal("What task would you like to add to your list?", Todo.new.prompt_for_task)
+  end
+
+  def test_input
+
+  end
 
 
 
