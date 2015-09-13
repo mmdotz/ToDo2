e3a8ed5a456714ad1bcc754c0a6ce073611d6f1b
Michelle Dotzenrod <dotzenrodm@gmail.com>
2015-09-12 19:09:01 -0500

    Easy mode done

--- bin/todo.rb
+++ bin/todo.rb
@@ -1,17 +1,25 @@
+require_relative "../db/setup"
+require_relative '../lib/all'
+
+
 class Todo
 
-  def add_todo
-    Todo.new(name: task)
-  end
+
 
   def prompt_for_task
     puts "What task would you like to add to your list?"
     task = gets.chomp
+    add_todo(task)
     puts "You have added #{task} to your list."
   end
+
+  def add_todo(task)
+    Todo.new(task: task).save
+  end
+
 end
 
 
-# Todo.new.prompt_for_task
+Todo.new.prompt_for_task  #this is not ineracting with the table
 
 
--- spec/testtodos.rb
+++ spec/testtodos.rb
@@ -6,21 +6,22 @@ require_relative '../bin/todo.rb'
 class TestTodos < Minitest::Test
 
   def test_class_todo
-    skip
+    # skip
     assert(Todo)
   end
 
   def test_add_todo
-    skip
+    # skip
     assert_equal("clean kitchen", Todo.new.add_todo("clean kitchen"))
   end
 
   def test_prompt
-    skip
+    # skip
     assert_equal("What task would you like to add to your list?", Todo.new.prompt_for_task)
   end
 
   def test_input
+    skip
 
   end
 
